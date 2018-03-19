80b62cca8e5e19d943e41eaa4d19295fee5dfe60
Ron Craig <ron.craig@comcast.net>
2018-03-19 00:13:12 -0700

    first commit, add js and spec.js file, pass five specs

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { HungryBear } from './../src/bear-buffet';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+$(document).ready(function() {
+
+});
