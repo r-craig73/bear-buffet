28b793185710ec3153323cc500d0ec606ccac3b4
Ron Craig <ron.craig@comcast.net>
2019-02-28 03:58:10 -0800

    edit jQuery commands, still under construction

--- src/main.js
+++ src/main.js
@@ -4,7 +4,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 $(document).ready(function() {
+  $('#name-form').submit(function(event) {
+  event.preventDefault();
+  const name = $("#name").val();
+  $("#name-form").hide();
+  $(".game").show();
 
+  let bear = new HungryBear(name);
+  bear.setHunger();
+
+  $("#bear-name").text(bear.name);
+  bear.displayResults();
+  bear.refreshPage();
+
+  $("#feed-meal").click(function() {
+    $("#food").text(bear.feed());
+    bear.refreshPage();
+   });
+
+  $("#pause-game").click(function() {
+
+  });
+
+  $("#wake-bear").click(function() {
+   });
+ });
 });
